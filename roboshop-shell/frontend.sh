999264f56d07975124140b5ca9c93c252dc4eaf0
vivekaps16 <vivekaps16@gmail.com>
2024-10-09 19:28:37 +0530

    frontend

---
+++ roboshop-shell/frontend.sh
@@ -0,0 +1,16 @@
+#Install Nginx
+dnf makecache
+dnf makecache
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
+#Start & Enable Nginx service
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+#Download the frontend content
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+#Extract the frontend content.
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+systemctl restart nginx
