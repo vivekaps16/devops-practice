f619b91e5b1f4b91543198e2820149d46be84c40
vivekaps16 <vivekaps16@gmail.com>
2024-10-10 17:50:24 +0530

    mongodb final

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,5 @@
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+set-prompt mongodb
+cp -r mongodb.repo /etc/yum.repos.d/mongodb.repo
 dnf install mongodb-org -y 
 systemctl enable mongod 
 systemctl restart mongod
