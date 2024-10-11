e306218c2ab6ecb326719bdd446521b053455904
vivekaps16 <vivekaps16@gmail.com>
2024-10-11 12:43:36 +0530

    correction of  '

--- roboshop-shell/redis.sh
+++ roboshop-shell/redis.sh
@@ -5,7 +5,6 @@ dnf module enable redis:7 -y
 
 dnf install redis -y 
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' -e '/protected-mode/ c '/protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' -e '/protected-mode/ c /protected-mode no' /etc/redis/redis.conf
 systemctl enable redis 
 systemctl start redis 
-
