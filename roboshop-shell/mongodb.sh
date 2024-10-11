2e79e355d8961b931b08f20858fe64c0e3928999
vivekaps16 <vivekaps16@gmail.com>
2024-10-11 15:36:09 +0530

    mongodb conf edit

--- roboshop-shell/mongodb.sh
+++ roboshop-shell/mongodb.sh
@@ -1,8 +1,8 @@
 set-prompt mongodb
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 cp -r mongodb.repo /etc/yum.repos.d/mongodb.repo
 dnf install mongodb-org -y 
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod 
 systemctl restart mongod
 systemctl status mongod ; tail -f /var/log/messages
