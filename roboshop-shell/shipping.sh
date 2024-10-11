300ce607d4a57a33ecbf7d805192a706526cb645
vivekaps16 <vivekaps16@gmail.com>
2024-10-11 13:10:32 +0530

    mysql ip update

--- roboshop-shell/shipping.sh
+++ roboshop-shell/shipping.sh
@@ -24,11 +24,11 @@ systemctl start shipping
 
 dnf install mysql -y 
 
-mysql -h shipping.devopspractice.info -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql.devopspractice.info -uroot -pRoboShop@1 < /app/db/schema.sql
 
-mysql -h shipping.devopspractice.info -uroot -pRoboShop@1 < /app/db/app-user.sql 
+mysql -h mysql.devopspractice.info -uroot -pRoboShop@1 < /app/db/app-user.sql 
 
-mysql -h shipping.devopspractice.info -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql.devopspractice.info -uroot -pRoboShop@1 < /app/db/master-data.sql
 
 systemctl restart shipping
 
